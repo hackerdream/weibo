8941a38227e4345f685a8b0e34d7d76a8bc7913e
GraySpider <76154662@qq.com>
2017-05-06 22:35:33 +0800

    modify weibo padding bug

--- src/weibo.vue
+++ src/weibo.vue
@@ -1,13 +1,17 @@
 <template>
 
-   <div id="weibo">
-       <w-nav class="nav"></w-nav>
-       <div class="main">
-           <w-content class="content"></w-content>
-           <login class="login"></login>
-       </div>
-   </div>
-
+    <div id="weibo">
+        <w-nav class="nav"></w-nav>
+        <div class="main">
+            <w-content class="content"></w-content>
+            <login class="login"></login>
+        </div>
+        <footer>
+            <p class="company">
+                <span>Copyright © 2017 weibo 信安三班某组</span>
+            </p>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -16,35 +20,41 @@
     import Login from './components/login.vue'
 
     export default {
-        components:{
-            WNav,WContent,Login
+        components: {
+            WNav, WContent, Login
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    *{
+    * {
         margin: 0;
         padding: 0;
     }
-    #weibo{
+
+    #weibo {
         padding-top: 60px;
         overflow: hidden;
         background-color: #d4d5e0;
-        background-image:url("../public/imgs/body_bg.jpg");
+        background-image: url("../public/imgs/body_bg.jpg");
         background-repeat: repeat-x;
         background-position: center 0;
     }
-    .main{
-        width: 1060px;
+
+    .main {
+        width: 1000px;
         margin: 0 auto;
+        overflow: hidden;
     }
-    @media screen and (max-width: 1050px){
-        .main{
-            width: 760px;
+
+    @media screen and (max-width: 1000px) {
+        .main {
+            width: 722px;
             margin: 0 auto;
+            overflow: hidden;
         }
     }
+
     .nav {
         display: block;
         position: fixed;
@@ -57,14 +67,16 @@
         background-color: #ffffff;
         overflow: hidden;
     }
-    .content{
+
+    .content {
         display: inline-block;
         position: relative;
         vertical-align: top;
         margin: 0 auto;
         z-index: 1;
     }
-    .login{
+
+    .login {
         margin-left: 20px;
         display: inline-block;
         position: relative;
@@ -74,9 +86,15 @@
         border: 1px solid #ccc;
 
     }
-    @media screen  and (max-width: 1050px){
-        .login{
+
+    @media screen  and (max-width: 1000px) {
+        .login {
             display: none;
         }
-    };
+    }
+    .company{
+        display: block;
+        width: 300px;
+        margin: 20px auto;
+    }
 </style>
